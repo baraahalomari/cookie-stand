478d557196665d87dc58d4af11dc935337689498
Baraah Alomari <balomari995@gmail.com>
2021-04-11 22:43:16 +0300

    fixing some problems

--- Javascript/app.js
+++ Javascript/app.js
@@ -30,12 +30,6 @@ function randomNumber(min, max) {
 }
 
 
-
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
 let locations = [];
 function LocationShop(nameLoc, min, max, avg) {
     this.nameLoc = nameLoc;
@@ -43,21 +37,21 @@ function LocationShop(nameLoc, min, max, avg) {
     this.max = max;
     this.min = min;
     this.avg = avg;
-    this.avgCust = [];
+    // this.avgCust = [];
     this.cookies = [];
     locations.push(this);
 }
 
 LocationShop.prototype.getAvg = function () {
     for (let j = 0; j < hoursLocation.length; j++) {
-        this.cookies.push(Math.floor(randomNumber(this.min, this.max)));
+        this.cookies.push(Math.floor(randomNumber(this.min, this.max) * this.avg));
         this.total += this.cookies[j];
     }
 
-    for (let a = 0; a < hoursLocation.length; a++) {
-        this.avgCust.push(Math.floor(randomNumber(this.min, this.max) * this.avg));
+    // for (let a = 0; a < hoursLocation.length; a++) {
+    //     this.avgCust.push(Math.floor(randomNumber(this.min, this.max) * this.avg));
         
-    }
+    // }
 }
 
 
@@ -82,12 +76,11 @@ LocationShop.prototype.randerRow = function () {
 }
 
 
-
-    let footerTr=document.createElement(`tr`);
-    table.appendChild(footerTr);
+function forFooter() {
+    let footrtER=document.createElement(`tr`);
+    table.appendChild(footrtER);
     let totalTd=document.createElement(`td`);
-    footerTr.appendChild(totalTd);
-
+    footrtER.appendChild(totalTd);
     totalTd.textContent=`Totals`;
 
     let totalForHour;
@@ -100,38 +93,33 @@ LocationShop.prototype.randerRow = function () {
             totalTotal+=locations[j].cookies[i];
         }
         let finalTd =document.createElement(`td`);
-
-        footerTr.appendChild(finalTd);
+        footrtER.appendChild(finalTd);
        finalTd.textContent=totalForHour;
-   
-
      }    
    
-    let totalsTd =document.createElement(`td`);
-    footerTr.appendChild(totalsTd);
-    totalsTd.textContent=totalTotal;
-
-
+    let totalsth =document.createElement(`th`);
+    footrtER.appendChild(totalsth);
+    totalsth.textContent=totalTotal;
 
 }
 forHeader();
 
 
 let seattle = new LocationShop(`seattle`, 23, 65, 6.3);
-seattle.getAvg();
+// seattle.getAvg();
 
 console.log(seattle.cookies);
 let tokyo = new LocationShop(`Tokyo`, 3, 24, 1.2);
-tokyo.getAvg();
+// tokyo.getAvg();
 
 let dubai = new LocationShop(`Dubai`, 11, 38, 3.7);
-dubai.getAvg();
+// dubai.getAvg();
 
 let paris = new LocationShop(`Paris`, 20, 38, 2.3);
-paris.getAvg();
+// paris.getAvg();
 
 let lima = new LocationShop(`Lima`, 2, 16, 4.6);
-lima.getAvg();
+// lima.getAvg();
 
 
 
@@ -157,13 +145,13 @@ cookieStand.addEventListener(`submit`,submitter);
 function submitter(event) {
     event.preventDefault();
     console.log(event);
-    let nameLoc=event.target.locationFaild.value;
+    let nameLoc= event.target.locationFaild.value;
     console.log(nameLoc);
-    let min =event.target.minCust.value;
+    let min = parseInt(event.target.minCust.value);
     console.log(min);
-    let max = event.target.maxCust.value;
+    let max = parseInt(event.target.maxCust.value);
     console.log(max);
-    let avg = event.target.avgCust.value;
+    let avg = parseFloat(event.target.avgCust.value);
     console.log(avgCust);
    let addedLocation=new LocationShop(nameLoc, min, max, avg);
     console.log(addedLocation);
@@ -174,7 +162,7 @@ function submitter(event) {
  
 
  for (let i = 0; i < locations.length; i++) {
-
+    locations[i].total=0;
          locations[i].getAvg();
         locations[i].randerRow();
         
@@ -189,7 +177,6 @@ for (let i = 0; i < locations.length; i++) {
     locations[i].randerRow();
      
 }
-
 
 forFooter();
 
